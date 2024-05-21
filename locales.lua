7310c35ffde1a75540fc2a49965471c961c472a5
Road-block <dridzt.addons@hotmail.com>
2024-05-21 19:34:18 +0300

    localization for custom stats still pending but at least we'll show the fallback English terms

--- locales.lua
+++ locales.lua
@@ -8,12 +8,10 @@ addon.L = L
 
 local LOCALE = GetLocale()
 
-if LOCALE == "enUS" then
-  L.STAT_CTC = "CTC"
-  L.STAT_LUCK = "Luck"
-  L.STAT_GEARCHECK = "Gear Checks"
-  L.STAT_CTC_DETAIL = "|TInterface\\TargetingFrame\\UI-TargetingFrame-Skull:0|tBoss CTC %s (%.2F%%)"
-  L.STAT_CTC_VERBOSE = "Combat Table Coverage"
-  L.STAT_CTC_DELTA = "CTC Delta"
-  L.STAT_CTC_CRITBLOCK = "|cff00ff00+%.2F%%|r |T236307:0|t"
-end
+L.STAT_CTC = "CTC"
+L.STAT_LUCK = "Luck"
+L.STAT_GEARCHECK = "Gear Checks"
+L.STAT_CTC_DETAIL = "|TInterface\\TargetingFrame\\UI-TargetingFrame-Skull:0|tBoss CTC %s (%.2F%%)"
+L.STAT_CTC_VERBOSE = "Combat Table Coverage"
+L.STAT_CTC_DELTA = "CTC Delta"
+L.STAT_CTC_CRITBLOCK = "|cff00ff00+%.2F%%|r |T236307:0|t"
